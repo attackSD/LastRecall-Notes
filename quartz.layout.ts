2694f59841d912425ce43325bef3359bfe3d02b5
YuSen-Wei <attackofsd123@gmail.com>
2026-08-05 00:38:54 +0800

    update: notes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -59,7 +59,15 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ConditionalRender({
+      component: Component.LanguageSwitcher(),
+      condition: (page) => !page.fileData.slug?.startsWith("tags"),
+    }),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
